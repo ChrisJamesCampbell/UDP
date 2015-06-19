d8598200bac9c7e6af21b1dbfe9a10e954e478e4
ChrisJamesCampbell <chris.campbell19@live.co.uk>
2015-06-19 14:21:55 +0100

    Update workbeast2.c

--- workbeast2.c
+++ workbeast2.c
@@ -1,1 +1,22 @@
+//test to put some strain on the CPU 
+//so that it its usage can be monitored
 
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h.>
+
+int main(void)
+{
+  printf("Start of test\n");
+  long double i = rand() / (long double)RAND_MAX;
+  
+  for(int c = i; i<1000; c++)
+  {
+    long double a = i * i
+  }
+  
+  printf("stop\n");
+
+  return 0;
+
+}
