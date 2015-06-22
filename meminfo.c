dc45fba972056f14ee65d672fedca360ab1cfcb6
ChrisJamesCampbell <chris.campbell19@live.co.uk>
2015-06-22 16:16:15 +0100

    Update meminfo.c

--- meminfo.c
+++ meminfo.c
@@ -21,17 +21,17 @@ int main(void)
 			
 			if(strncmp(MEMTOTAL, line, 8) == 0)
 			{
-				scanf(line, "%s %f", &mem_total);
+				scanf(line, "*%s %f", &mem_total);
 			}
 			
 			if(strncmp(SHARED, line, 8) == 0)
 			{
-				scanf(line, "%s %f", &shared);
+				scanf(line, "*%s %f", &shared);
 			}
 			
 			if(strncmp(CACHED, line, 7) == 0)
 			{
-				scanf(line, "%s %f", &cached);
+				scanf(line, "*%s %f", &cached);
 			}
 		}
 		
