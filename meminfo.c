a44b58e8e2573d1d48d9aab5698544b0443c437a
ChrisJamesCampbell <chris.campbell19@live.co.uk>
2015-06-22 16:13:12 +0100

    Update meminfo.c

--- meminfo.c
+++ meminfo.c
@@ -11,7 +11,7 @@ int main(void)
 	FILE *fp;
 	char line[256];
 	
-	double mem_total, shared, cached = 0;
+	double mem_total, shared, cached = 0.0;
 	
 	
 	fp = fopen("/proc/meminfo","r");
@@ -21,17 +21,17 @@ int main(void)
 			
 			if(strncmp(MEMTOTAL, line, 8) == 0)
 			{
-				scanf(line, "%s %f", mem_total);
+				scanf(line, "%s %f", &mem_total);
 			}
 			
 			if(strncmp(SHARED, line, 8) == 0)
 			{
-				scanf(line, "%s %f", shared);
+				scanf(line, "%s %f", &shared);
 			}
 			
 			if(strncmp(CACHED, line, 7) == 0)
 			{
-				scanf(line, "%s %f", cached);
+				scanf(line, "%s %f", &cached);
 			}
 		}
 		
