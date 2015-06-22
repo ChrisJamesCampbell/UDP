dc333331fb56e61c5160bfed88577ca6852f7c84
ChrisJamesCampbell <chris.campbell19@live.co.uk>
2015-06-22 09:59:51 +0100

    Update listener.c
    Changed the final printf line to format the 'buf' printing as an ascii decimal rather than an ascii char

--- listener.c
+++ listener.c
@@ -91,7 +91,7 @@ int main(void)
                 s, sizeof s));
         printf("listener: packet is %d bytes long\n", numbytes);
         buf[numbytes] = '\0';
-        printf("listener: packet contains \"%s\"\n", buf);
+        printf("listener: packet contains \"%d\"\n", buf);
 
     }
     close(sockfd);
