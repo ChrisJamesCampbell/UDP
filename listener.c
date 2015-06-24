70a0e71132a8719563b1428e14cb4237293582cd
ChrisJamesCampbell <chris.campbell19@live.co.uk>
2015-06-24 14:22:50 +0100

    Update listener.c

--- listener.c
+++ listener.c
@@ -108,7 +108,7 @@ int main(void)
                 continue;
             }
         
-        if(numbytes != (sizeof(struct sysinfo))
+        if(numbytes != (sizeof(struct sysinfo)))
         {
             sleep(1);
             continue;
