e417f0e979ac31de18bc2409c692de2da19fcdbf
ChrisJamesCampbell <chris.campbell19@live.co.uk>
2015-06-25 11:03:36 +0100

    Update doublelink.c

--- doublelink.c
+++ doublelink.c
@@ -1,137 +1,87 @@
-#include <stdlib.h>
-
-struct ListNode;
-
-typedef struct ListNode {
-    struct ListNode *next;
-    struct ListNode *prev;
-    void *value;
-} ListNode;
-
-typedef struct List {
-    int count;
-    ListNode *first;
-    ListNode *last;
-} List;
-
-List *List_create()
-{
-    return calloc(1, sizeof(List));
+/* Doubly Linked List implementation */
+#include<stdio.h>
+#include<stdlib.h>
+ 
+struct Node  {
+	int data;
+	struct Node* next;
+	struct Node* prev;
+};
+ 
+struct Node* head; // global variable - pointer to head node.
+ 
+//Creates a new Node and returns pointer to it. 
+struct Node* GetNewNode(int x) {
+	struct Node* newNode
+		= (struct Node*)malloc(sizeof(struct Node));
+	newNode->data = x;
+	newNode->prev = NULL;
+	newNode->next = NULL;
+	return newNode;
 }
-
-void List_destroy(List *list)
-{
-    LIST_FOREACH(list, first, next, cur) {
-        if(cur->prev) {
-            free(cur->prev);
-        }
-    }
-
-    free(list->last);
-    free(list);
+ 
+//Inserts a Node at head of doubly linked list
+void InsertAtHead(int x) {
+	struct Node* newNode = GetNewNode(x);
+	if(head == NULL) {
+		head = newNode;
+		return;
+	}
+	head->prev = newNode;
+	newNode->next = head; 
+	head = newNode;
 }
-
-
-void List_clear(List *list)
-{
-    LIST_FOREACH(list, first, next, cur) {
-        free(cur->value);
-    }
+ 
+//Inserts a Node at tail of Doubly linked list
+void InsertAtTail(int x) {
+	struct Node* temp = head;
+	struct Node* newNode = GetNewNode(x);
+	if(head == NULL) {
+		head = newNode;
+		return;
+	}
+	while(temp->next != NULL) temp = temp->next; // Go To last Node
+	temp->next = newNode;
+	newNode->prev = temp;
 }
-
-
-void List_clear_destroy(List *list)
-{
-    List_clear(list);
-    List_destroy(list);
+ 
+//Prints all the elements in linked list in forward traversal order
+void Print() {
+	struct Node* temp = head;
+	printf("Forward: ");
+	while(temp != NULL) {
+		printf("%d ",temp->data);
+		temp = temp->next;
+	}
+	printf("\n");
 }
-
-
-void List_push(List *list, void *value)
-{
-    ListNode *node = calloc(1, sizeof(ListNode));
-    check_mem(node);
-
-    node->value = value;
-
-    if(list->last == NULL) {
-        list->first = node;
-        list->last = node;
-    } else {
-        list->last->next = node;
-        node->prev = list->last;
-        list->last = node;
-    }
-
-    list->count++;
-
-error:
-    return;
+ 
+//Prints all elements in linked list in reverse traversal order. 
+void ReversePrint() {
+	struct Node* temp = head;
+	if(temp == NULL) return; // empty list, exit
+	// Going to last Node
+	while(temp->next != NULL) {
+		temp = temp->next;
+	}
+	// Traversing backward using prev pointer
+	printf("Reverse: ");
+	while(temp != NULL) {
+		printf("%d ",temp->data);
+		temp = temp->prev;
+	}
+	printf("\n");
 }
-
-void *List_pop(List *list)
-{
-    ListNode *node = list->last;
-    return node != NULL ? List_remove(list, node) : NULL;
-}
-
-void List_unshift(List *list, void *value)
-{
-    ListNode *node = calloc(1, sizeof(ListNode));
-    check_mem(node);
-
-    node->value = value;
-
-    if(list->first == NULL) {
-        list->first = node;
-        list->last = node;
-    } else {
-        node->next = list->first;
-        list->first->prev = node;
-        list->first = node;
-    }
-
-    list->count++;
-
-error:
-    return;
-}
-
-void *List_shift(List *list)
-{
-    ListNode *node = list->first;
-    return node != NULL ? List_remove(list, node) : NULL;
-}
-
-void *List_remove(List *list, ListNode *node)
-{
-    void *result = NULL;
-
-    check(list->first && list->last, "List is empty.");
-    check(node, "node can't be NULL");
-
-    if(node == list->first && node == list->last) {
-        list->first = NULL;
-        list->last = NULL;
-    } else if(node == list->first) {
-        list->first = node->next;
-        check(list->first != NULL, "Invalid list, somehow got a first that is NULL.");
-        list->first->prev = NULL;
-    } else if (node == list->last) {
-        list->last = node->prev;
-        check(list->last != NULL, "Invalid list, somehow got a next that is NULL.");
-        list->last->next = NULL;
-    } else {
-        ListNode *after = node->next;
-        ListNode *before = node->prev;
-        after->prev = before;
-        before->next = after;
-    }
-
-    list->count--;
-    result = node->value;
-    free(node);
-
-error:
-    return result;
+ 
+int main() {
+ 
+	/*Driver code to test the implementation*/
+	head = NULL; // empty list. set head as NULL. 
+	
+	// Calling an Insert and printing list both in forward as well as reverse direction. 
+	InsertAtTail(2); Print(); ReversePrint();
+	InsertAtTail(4); Print(); ReversePrint();
+	InsertAtHead(6); Print(); ReversePrint();
+	InsertAtTail(8); Print(); ReversePrint();
+	
 }
