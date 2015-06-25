921079648de265cbfafc7e55df7f3d0e1be31557
ChrisJamesCampbell <chris.campbell19@live.co.uk>
2015-06-25 09:01:03 +0100

    Update ramrage.c

--- ramrage.c
+++ ramrage.c
@@ -1,10 +1,36 @@
 #include <stdlib.h>
+#include <string.h>
+#define UNIX 1
 
-int main() {
-int *p;
-while(1) {
-    int inc=1024*1024*sizeof(char);
-    p=(int*) calloc(1,inc);
-    if(!p) break;
+//remove the above line if running under Windows
+
+#ifdef UNIX
+    #include <unistd.h>
+#else
+    #include <windows.h>
+#endif
+
+int main(int argc, char** argv)
+{
+    unsigned long mem;
+    if(argc==1)
+        mem = 1024*1024*512; //512 mb
+    else if(argc==2)
+        mem = (unsigned) atol(argv[1]);
+    else
+    {
+        printf("Usage: loadmem <memory in bytes>");
+        exit(1);
+    }
+
+    char* ptr = malloc(mem);
+    while(1)
+    {
+        memset(ptr, 0, mem);
+        #ifdef UNIX
+            sleep(120);
+        #else
+            Sleep(120*1000);
+        #endif
     }
 }
