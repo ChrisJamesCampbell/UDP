27369315b4ddba1f2b39bde6d5c7e0362960838f
ChrisJamesCampbell <chris.campbell19@live.co.uk>
2015-06-29 09:23:38 +0100

    Update listener2.c

--- listener2.c
+++ listener2.c
@@ -105,8 +105,8 @@ int main(void)
     
     struct sys_info *new_packet = (struct sys_info *)buf;
     
-    save_data(struct sys_info *old_data, struct new_sys_info *new_data);
-    
+   // save_data(struct sys_info *old_data, struct new_sys_info *new_data);
+   save_data(&old_data, &new_data);
 
 
     memset(&hints, 0, sizeof hints);
