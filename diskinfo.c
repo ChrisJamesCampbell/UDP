1e252df34c6de83db4a63ffaaabc52cb32463b6f
ChrisJamesCampbell <chris.campbell19@live.co.uk>
2015-06-29 15:20:27 +0100

    Update diskinfo.c

--- diskinfo.c
+++ diskinfo.c
@@ -5,9 +5,9 @@ int main(void)
 {
   FILE *fp;
 	char line[256];
-	double disk_writes = 0.0;
-	double disk_reads = 0.0;
-	double disk_activity = 0.0;
+	int disk_writes = 0.0;
+	int disk_reads = 0.0;
+	int disk_activity = 0.0;
 	int count = 0;
 	
 	
@@ -18,19 +18,19 @@ int main(void)
 		{
 			if(count == 4)
 			{
-				fscanf(fp, "%f", &disk_writes);//extract contents of line 4
+				fscanf(fp, "%d", &disk_writes);//extract contents of line 4
 			}
 			
 			if(count == 8)
 			{
-				fscanf(fp, "%f", &disk_reads);	//extract contents of line 8
+				fscanf(fp, "%d", &disk_reads);	//extract contents of line 8
 			}
 		count++;
 		}
 		
 	disk_activity = disk_writes + disk_reads;
 	
-	printf("The disk activity was: %f ", disk_activity);
+	printf("The disk activity was: %d ", disk_activity);
 	
 	fclose(fp);
 }
