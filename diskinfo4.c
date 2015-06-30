0bff4e181fb59c26ec52bee3c6e3753e356fe495
ChrisJamesCampbell <chris.campbell19@live.co.uk>
2015-06-30 14:23:58 +0100

    Update diskinfo4.c

--- diskinfo4.c
+++ diskinfo4.c
@@ -11,15 +11,20 @@ int main(void)
 	
 	int disk_writes;
 	int disk_writes_total;
+	int count = 1;
 	
-	int disk_activity = 0.0;
+	int[] disk_activity[2];
+	
+	int relative_activity = 0;
 	
 	
 	fp = fopen("/proc/diskstats","r");
 	
-	
-		while(fgets(line,256, fp))
+		while(count < 3)
 		{
+			
+		while(fgets(line,256, fp))
+		
 			{
 				//pulls the 'Reads Completed' successfully column for an sda and adds to total
 				sscanf(line,"%*[ ]%*d%*[ ]%*d%*[ ]%*s%*[ ]%d" ,&disk_reads);
@@ -28,16 +33,21 @@ int main(void)
 				//pulls the 'Writes Completed' successfully column for an sda and adds to total
 				sscanf(line,"%*[ ]%*d%*[ ]%*d%*[ ]%*s%*[ ]%*d%*[ ]%*d%*[ ]%*d%*[ ]%*d%*[ ]%d", &disk_writes);
 				disk_writes_total = disk_writes_total + disk_writes;
+				
+		
 			}
 			
-			
+		disk_activity[count] = disk_reads_total + disk_writes_total;
+		count++;
+		sleep(5);
+		
 		}
 		
-	disk_activity = disk_reads_total + disk_writes_total;
+	relative_activity = disk_activity[2] - disck_activity[1];
 	
 	printf("\nThe total number of disk reads was: %d \n", disk_reads_total);
 	printf("The total number of disk writes was: %d \n", disk_writes_total);
-	printf("The total disk activity was: %d \n \n", disk_activity);
+	printf("The relative disk activity was: %d \n \n", relative_activity);
 	
 	fclose(fp);
 }
