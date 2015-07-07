2b3ce1c58ea68ea7a4009c904871ff62097c6c27
ChrisJamesCampbell <chris.campbell19@live.co.uk>
2015-07-07 12:03:51 +0100

    Update devbytes.c

--- devbytes.c
+++ devbytes.c
@@ -10,6 +10,10 @@ int main(void)
 	double total_received_bytes = 0.0;
 	double total_transmitted_bytes = 0.0;
 	
+	double network_activity[2];
+	
+	
+	
 	
 	int count = 1;
 	
@@ -18,22 +22,34 @@ int main(void)
 	
 	fp = fopen("/proc/net/dev","r");
 	
-	while(fgets(line,256, fp))
-	{
-		//pulls the receieved bytes information which is the first column of integers
-		//within net/dev
-		sscanf(line, "%*[ ]%*s%*[ ]%lf", &received_bytes);
-		total_received_bytes = total_received_bytes + received_bytes;
+	
+	while(count < 3)
+		{
+		while(fgets(line,256, fp))
+		{
+			//pulls the receieved bytes information which is the first column of integers
+			//within net/dev
+			sscanf(line, "%*[ ]%*s%*[ ]%lf", &received_bytes);
+			total_received_bytes = total_received_bytes + received_bytes;
+			
+			
+			//pulls the transmitted bytes information which is represented by the 9th column of 
+			//integers within net/dev
+			sscanf(line, "%*[ ]%*s%*[ ]%*lf%*[ ]%*lf%*[ ]%*lf%*[ ]%*lf%*[ ]%*lf%*[ ]%*lf%*[ ]%*lf%*[ ]%*lf%*[ ]%lf",
+			&transmitted_bytes);
+			total_transmitted_bytes = total_transmitted_bytes + transmitted_bytes;
+			
+			
+		}
 		
+		network_activity[count] = total_received_bytes + total_transmitted_bytes;
+		count++;
+	    fclose(fp);
 		
-		//pulls the transmitted bytes information which is represented by the 9th column of 
-		//integers within net/dev
-		sscanf(line, "%*[ ]%*s%*[ ]%*lf%*[ ]%*lf%*[ ]%*lf%*[ ]%*lf%*[ ]%*lf%*[ ]%*lf%*[ ]%*lf%*[ ]%*lf%*[ ]%lf",
-		&transmitted_bytes);
-		total_transmitted_bytes = total_transmitted_bytes + transmitted_bytes;
-		
-		
+		sleep(1);
 	}
+	
+	double relative_network_activity = network_activity[2] - network_activity[1];
 	
 	printf("\n The total number of bytes received was: %lf", total_received_bytes);
 	printf("\n The total number of bytes transmitted was: %lf\n", total_transmitted_bytes);
