3e21d9db790ebf7331dc0c40f41ea3cb9cc31418
ChrisJamesCampbell <chris.campbell19@live.co.uk>
2015-07-07 12:22:24 +0100

    Update devbytes.c

--- devbytes.c
+++ devbytes.c
@@ -3,74 +3,77 @@
 
 int main(void)
 {
-	
-	double received_bytes = 0.0;
-	double transmitted_bytes = 0.0;
-	
-	double total_received_bytes = 0.0;
-	double total_transmitted_bytes = 0.0;
-	
-	double network_activity[2];
-	
-	
-	
-	
-	int count = 1;
-	
-	FILE *fp;
-	char line[256];
-	
-	
-	
-	while(count < 3)
-		{
-		    fp = fopen("/proc/net/dev","r");
-		    
-			while(fgets(line,256, fp))
+	while(1)
+		
+	{
+		
+		double received_bytes = 0.0;
+		double transmitted_bytes = 0.0;
+		
+		double total_received_bytes = 0.0;
+		double total_transmitted_bytes = 0.0;
+		
+		double network_activity[2];
+		
+		
+		
+		
+		int count = 1;
+		
+		FILE *fp;
+		char line[256];
+		
+		
+		
+		while(count < 3)
 			{
-				//pulls the receieved bytes information which is the first column of integers
-				//within net/dev
-				sscanf(line, "%*[ ]%*s%*[ ]%lf", &received_bytes);
-				total_received_bytes = total_received_bytes + received_bytes;
-				
-				
-				//pulls the transmitted bytes information which is represented by the 9th column of 
-				//integers within net/dev
-				sscanf(line, "%*[ ]%*s%*[ ]%*lf%*[ ]%*lf%*[ ]%*lf%*[ ]%*lf%*[ ]%*lf%*[ ]%*lf%*[ ]%*lf%*[ ]%*lf%*[ ]%lf",
-				&transmitted_bytes);
-				total_transmitted_bytes = total_transmitted_bytes + transmitted_bytes;
-				
-				
-			}
+			    fp = fopen("/proc/net/dev","r");
+			    
+				while(fgets(line,256, fp))
+				{
+					//pulls the receieved bytes information which is the first column of integers
+					//within net/dev
+					sscanf(line, "%*[ ]%*s%*[ ]%lf", &received_bytes);
+					total_received_bytes = total_received_bytes + received_bytes;
+					
+					
+					//pulls the transmitted bytes information which is represented by the 9th column of 
+					//integers within net/dev
+					sscanf(line, "%*[ ]%*s%*[ ]%*lf%*[ ]%*lf%*[ ]%*lf%*[ ]%*lf%*[ ]%*lf%*[ ]%*lf%*[ ]%*lf%*[ ]%*lf%*[ ]%lf",
+					&transmitted_bytes);
+					total_transmitted_bytes = total_transmitted_bytes + transmitted_bytes;
+					
+					
+				}
+			
+		    network_activity[count] = total_received_bytes + total_transmitted_bytes;
+		    count++;
+		    fclose(fp);
+		    
+		   if(count == 3)
+		   {
+		   	break;
+		   }
+		    
+		   //resets total for next loop
+		   total_received_bytes = 0.0;
+		   total_transmitted_bytes = 0.0;
+			
+		   //sleep for one second in order to give us 
+		   //network activity in terms of bytes/second
+		sleep(1);
+		}
 		
-	    network_activity[count] = total_received_bytes + total_transmitted_bytes;
-	    count++;
-	    fclose(fp);
-	    
-	   if(count == 3)
-	   {
-	   	break;
-	   }
-	    
-	   //resets total for next loop
-	   total_received_bytes = 0.0;
-	   total_transmitted_bytes = 0.0;
+		double relative_network_activity = network_activity[2] - network_activity[1];
+		double bandwidth = relative_network_activity / 8;
 		
-	   //sleep for one second in order to give us 
-	   //network activity in terms of bytes/second
-	sleep(1);
+		printf("\n The total number of bytes received was: %lf", total_received_bytes);
+		printf("\n The total number of bytes transmitted was: %lf", total_transmitted_bytes);
+		printf("\n The relative network activity was: %lf Bytes", relative_network_activity);
+		printf("\n The bandwidth was: %lf bits per second\n", bandwidth);
+		
+	
+	
 	}
-	
-	double relative_network_activity = network_activity[2] - network_activity[1];
-	double bandwidth = relative_network_activity / 8;
-	
-	printf("\n The total number of bytes received was: %lf", total_received_bytes);
-	printf("\n The total number of bytes transmitted was: %lf", total_transmitted_bytes);
-	printf("\n The relative network activity was: %lf Bytes", relative_network_activity);
-	printf("\n The bandwidth was: %lf bits per second\n", bandwidth);
-	
-	
-	
-	
 	
 }
