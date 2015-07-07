371aea9373f166c44de456a460e46293b64f1810
ChrisJamesCampbell <chris.campbell19@live.co.uk>
2015-07-07 11:13:53 +0100

    Update devbytes.c

--- devbytes.c
+++ devbytes.c
@@ -4,13 +4,39 @@
 int main(void)
 {
 	
+	double received_bytes = 0.0;
+	double transmitted_bytes = 0.0;
+	
+	double total_received_bytes = 0.0;
+	double total_transmitted_bytes = 0.0;
+	
+	
+	int count = 1;
+	
 	FILE *fp;
 	char line[256];
 	
-	fp = fopen("/proc/meminfo","r");
+	fp = fopen("/proc/net/dev","r");
 	
-	double receive_bytes = 0.0;
-	double transmit_bytes = 0.0;
+	while(fgets(line,256, fp))
+	{
+		//pulls the receieved bytes information which is the first column of integers
+		//within net/dev
+		sscanf(line, "%*[ ]%*s%*[ ]%lf", &received_bytes);
+		total_received_bytes = total_received_bytes + received_bytes;
+		
+		sscanf(line, "%*[ ]%*s%*[ ]%*lf%*[ ]%*lf%*[ ]%*lf%*[ ]%*lf%*[ ]%*lf%*[ ]%*lf%*[ ]%*lf%*[ ]%*lf%*[ ]%*lf%*[ ]%lf",
+		&transmitted_bytes);
+		total_transmitted_bytes = total_transmitted_bytes + transmitted_bytes;
+		
+		
+	}
+	
+	printf("\n The total number of bytes received was: %lf", total_received_bytes);
+	printf("\n The total number of bytes transmitted was: %lf\n");
+	
+	
+	
 	
 	
 }
