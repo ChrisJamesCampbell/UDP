3e1255dc9c5f09ab7bbe4fad18a4b6f08dfc6a5b
ChrisJamesCampbell <chris.campbell19@live.co.uk>
2015-07-08 12:26:23 +0100

    Update and rename talker3.c to talker3.1.c

--- talker3.1.c
+++ talker3.1.c
@@ -282,7 +282,7 @@ static void find_bandwidth(struct sysinfo_type *sysinfo)
 			sysinfo->proportional_bandwidth = ((sysinfo->instantaneous_bandwidth/peak_bandwidth) * 100);
 		}
 }
-
+ /*
 static void find_ip_addresses()
 {
 	FILE *fp;
@@ -301,7 +301,7 @@ static void find_ip_addresses()
 		}
 	}
 }
-
+*/
 
 
 int main(int argc, char *argv[])
@@ -329,39 +329,52 @@ int main(int argc, char *argv[])
             fprintf(stderr,"usage: talker hostname message\n");
             exit(1);
         }
+        
+        FILE *fp;
+		char line[256];
+		char ip_to_send[40];
+		
+		fp = fopen("/UDP/ip_addresses.txt","r");
+	
+	    while(fgets(line,256, fp))
+	    {
+	    	sscanf(line, "%s", ip_to_send);
+	    	memset(&hints, 0, sizeof hints);
+	    	
+	        hints.ai_family = AF_UNSPEC;
+	        hints.ai_socktype = SOCK_DGRAM;
+	    
+	        if ((rv = getaddrinfo(ip_to_send, SERVERPORT, &hints, &servinfo)) != 0) {
+	            fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+	            return 1;
+	        }
+	    
+	        // loop through all the results and make a socket
+	        for(p = servinfo; p != NULL; p = p->ai_next) {
+	            if ((sockfd = socket(p->ai_family, p->ai_socktype,
+	                    p->ai_protocol)) == -1) {
+	                perror("talker: socket");
+	                continue;
+	            }
+	    
+	            break;
+	        }
+	    
+	        if (p == NULL) {
+	            fprintf(stderr, "talker: failed to bind socket\n");
+	            return 2;
+	        }
+	    
+	        if ((numbytes = sendto(sockfd, &sysinfo, sizeof(struct sysinfo_type), 0,
+	                 p->ai_addr, p->ai_addrlen)) == -1) {
+	            perror("talker: sendto");
+	            exit(1);
+	        }
+	    
+	        freeaddrinfo(servinfo);
+	    }
     
-        memset(&hints, 0, sizeof hints);
-        hints.ai_family = AF_UNSPEC;
-        hints.ai_socktype = SOCK_DGRAM;
-    
-        if ((rv = getaddrinfo(argv[1], SERVERPORT, &hints, &servinfo)) != 0) {
-            fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-            return 1;
-        }
-    
-        // loop through all the results and make a socket
-        for(p = servinfo; p != NULL; p = p->ai_next) {
-            if ((sockfd = socket(p->ai_family, p->ai_socktype,
-                    p->ai_protocol)) == -1) {
-                perror("talker: socket");
-                continue;
-            }
-    
-            break;
-        }
-    
-        if (p == NULL) {
-            fprintf(stderr, "talker: failed to bind socket\n");
-            return 2;
-        }
-    
-        if ((numbytes = sendto(sockfd, &sysinfo, sizeof(struct sysinfo_type), 0,
-                 p->ai_addr, p->ai_addrlen)) == -1) {
-            perror("talker: sendto");
-            exit(1);
-        }
-    
-        freeaddrinfo(servinfo);
+        
     
         /*printf("talker: sent %d bytes to %s containing %d and %f and %d and %d and %lf\n", numbytes, argv[1], 
         sysinfo.cpu_load, sysinfo.free_mem, sysinfo.disk_activity, sysinfo.proportional_activity,
