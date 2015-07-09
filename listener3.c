6d08082a0d26ecbeabd1c2678d8ed0db55aa35b8
ChrisJamesCampbell <chris.campbell19@live.co.uk>
2015-07-09 15:13:44 +0100

    Update listener3.c

--- listener3.c
+++ listener3.c
@@ -262,21 +262,22 @@ int main(void)
         
         fp = fopen("/root/UDP/stored_sys_info", "w");
         
-        fprintf(fp, "{\n \t \"CPU Load\" : \" %d \" ,"
-                  " \n \t \"Proportional Free Memory\" : \"%lf\" ,"
-                  " \n \t \"Proportional Disk Activity\" : \"%d\" ,"
-                  " \n \t \"Proportional Bandwidth\" : \"%lf\" ,"
-                  " \n \t \"Roles\" : [ "
-                  " \n \t \t {\"Role ID\" : \"1\" , "
-                  " \n \t \t \"CPU Load\" : \"%d\" , "
-                  " \n \t \t \"Proportional Free Memory\" : \"%lf\" ,"
-                  " \n \t \t \"Proportional Disk Activity\" : \"%lf\" ,"
-                  " \n \t \t \"Proportional Bandwidth\" : \"%lf\" }"
+        fprintf(fp, "{\n \t \"cpu_load\" : \" %d \" ,"
+                  " \n \t \"proportional_free_mem\" : %lf ,"
+                  " \n \t \"proportional_disk_activity\" : %d ,"
+                  " \n \t \"proportional_bandwidth\" : %lf ,"
+                  " \n \t \"roles\" : [ "
+                  " \n \t \t {\"role_id\" : 1 , "
+                  " \n \t \t \"cpu_load\" : %d , "
+                  " \n \t \t \"proportional_free_memory\" : %lf ,"
+                  " \n \t \t \"proportional_disk_activity\" : %lf ,"
+                  " \n \t \t \"proportional_bandwidth\" : %lf }"
                   " \n \t \t   ]"
                   "\n }", 
                (int)old_data.cpu_load, new_packet->proportional_free_mem,
                new_packet->proportional_disk_activity, new_packet->proportional_bandwidth,
-               old_br_data.cpu_load);
+               old_br_data.cpu_load, old_br_data.proportional_free_mem, 
+               old_br_data.proportional_disk_activity, old_br_data.proportional_bandwidth);
         
         fclose(fp);
         
